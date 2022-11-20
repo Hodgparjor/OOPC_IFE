0dac15fc0bd4c827b539b952a4865239a84eef3e
Aleksander Brzostowski <olekbrzost@gmail.com>
2022-11-20 16:26:52 +0100

    Everything except multiplication finished

--- PolynomialClass/Poly.cpp
+++ PolynomialClass/Poly.cpp
@@ -1,17 +1,15 @@
 #include "Poly.h"
 #include <iterator>
+#include <string>
+#include <math.h>
 
 using namespace std;
+
+// --- CONSTRUCTORS ---
 
 PolyElement::PolyElement(double coefficent, int degree) {
     this->coefficent = coefficent;
     this->degree = degree;
-}
-
-PolyElement& PolyElement::operator=(double newCoefficent) {
-    this->coefficent = newCoefficent;
-    cout << this->coefficent << endl;
-    return *this;
 }
 
 Poly::Poly() {
@@ -19,63 +17,178 @@ Poly::Poly() {
 }
 
 Poly::Poly(double coefficent) {
-    this->numberOfElements = 1;
-    this->elements.push_back(PolyElement(coefficent));
+    Poly newPoly;
+    newPoly[0] = coefficent;
+    this->elements = newPoly.elements;
+    this->numberOfElements = newPoly.numberOfElements;
 }
 
-Poly::Poly(const Poly& p) {}
+Poly::Poly(const Poly& p) {
+    this->numberOfElements = p.numberOfElements;
+    this->elements = list<PolyElement>(p.elements);
+}
+
+// --- OPERATOR OVERLOADING ---
+
+PolyElement& PolyElement::operator=(double newCoefficent) {
+    this->coefficent = newCoefficent;
+    return *this;
+}
 
 PolyElement& Poly::operator[](int insertedDegree) {
-    PolyElement newElement(0, insertedDegree);
     if(this->numberOfElements == 0) {
+        PolyElement newElement(0, insertedDegree);
         this->elements.push_back(newElement);
         this->numberOfElements++;
         return (elements.front());
     }
     list<PolyElement>::iterator it;
-    for(it = elements.begin(); it != elements.end(); ++it) {
-        if((*it).degree < insertedDegree) {
+    bool inserted = false;
+    for(it = elements.begin(); it != elements.end() && inserted == false; ++it) {
+        if ((*it).degree == insertedDegree) {
+            inserted = true;
+            return (*it);
+        } else if((*it).degree < insertedDegree) {
+            inserted = true;
+            PolyElement newElement(0, insertedDegree);
             elements.insert(it, newElement);
             numberOfElements++;
-            return (*it);
+            return (*(--it));
         }
     }
+    PolyElement newElement(0, insertedDegree);
     elements.push_back(newElement);
     numberOfElements++;
     return (elements.back());
 }
 
-// void Poly::display() {
-//     list<PolyElement>::iterator it;
-//     char sign;
-//     for(it = elements.begin(); it != elements.end(); ++it) {
-//         sign = '+';
-//         if((*it).coefficent < 0) {
-//             sign = '-';
-//         }
-//         cout << " \n" << sign << " " << abs((*it).coefficent) << "^" << (*it).degree;
-//     }
-// }
-
-ostream& operator<<(ostream& out, const Poly& poly){
-    list<PolyElement>::const_iterator it;
-    char sign;
-    for(it = poly.elements.begin(); it != poly.elements.end(); ++it) {
-        sign = '+';
-        if((*it).coefficent < 0) {
-            sign = '-';
-        }
-        if((&(*it) == &(poly.elements.front()))) {
-            if((*it).coefficent >= 0) {
-                out << " " << abs((*it).coefficent);
-            } else {
-                out << sign << " " << abs((*it).coefficent);
+Poly operator+(const Poly& p1, const Poly& p2) {
+    Poly newPoly(p1);
+    list<PolyElement>::const_iterator it2;
+    list<PolyElement>::iterator it1;
+    bool foundMatch = false;
+    for(it2 = p2.elements.begin(); it2!= p2.elements.end(); ++it2) {
+        foundMatch = false;
+        for(it1 = newPoly.elements.begin(); it1!= newPoly.elements.end() && !foundMatch; ++it1) {
+            if((*it2).degree == (*it1).degree) {
+                newPoly[(*it2).degree] = (*it1).coefficent + (*it2).coefficent;
+                foundMatch = true;
             }
         }
-        if(((*it).degree == 0)) {
-            out << " " << sign << " " << abs((*it).coefficent);
-        } else {
-            out << " " << sign << " " << abs((*it).coefficent) << "x^" << (*it).degree;
+        if(!foundMatch) {
+            newPoly[(*it2).degree] = (*it2).coefficent;
+        }
+    }
+    return newPoly;
+}
+
+Poly operator-(const Poly& p1, const Poly& p2) {
+    Poly newPoly(p1);
+    list<PolyElement>::const_iterator it2;
+    list<PolyElement>::iterator it1;
+    bool foundMatch = false;
+    for(it2 = p2.elements.begin(); it2!= p2.elements.end(); ++it2) {
+        foundMatch = false;
+        for(it1 = newPoly.elements.begin(); it1!= newPoly.elements.end() && !foundMatch; ++it1) {
+            if((*it2).degree == (*it1).degree) {
+                newPoly[(*it2).degree] = (*it1).coefficent - (*it2).coefficent;
+                foundMatch = true;
+            }
+        }
+        if(!foundMatch) {
+            newPoly[(*it2).degree] = -(*it2).coefficent;
+        }
+    }
+    return newPoly;
+}
+
+Poly Poly::operator-() const {
+    Poly newPoly;
+    list<PolyElement>::const_iterator it;
+    for(it = this->elements.begin(); it != this->elements.end(); ++it) {
+        newPoly[(*it).degree] = -(*it).coefficent;
+    }
+    return newPoly;
+}
+
+double Poly::operator()(double xval) const {
+    double result;
+    list<PolyElement>::const_iterator it;
+    for(it = this->elements.begin(); it != this->elements.end(); ++it) {
+        result += (pow(xval, (*it).degree) * (*it).coefficent);
+    }
+    return result;
+}
+// --- OUTPUT STREAM ---
+
+list<PolyElement> getListToPrint(const Poly& poly) {
+    list<PolyElement>::const_iterator it;
+    list<PolyElement> outputList;
+    int numberOfElements = 0;
+    for(it = poly.elements.begin(); it != poly.elements.end(); ++it) {
+        if(!((*it).coefficent == 0)) {
+            outputList.push_back((*it));
+            numberOfElements++;
+        }
+    }
+    if((numberOfElements == 0) && (poly.elements.front().degree == 0)) {
+        outputList.push_front(poly.elements.front());
+    }
+    return outputList;
+}
+
+string BaseOutputElement(const PolyElement& element) {
+    string output;
+    if(element.coefficent != 1) {
+        output = to_string(abs(element.coefficent));
+    }
+    output.erase(output.find_last_not_of('0') + 1, string::npos);
+    output.erase(output.find_last_not_of('.') + 1, string::npos);
+    if(element.degree == 1) {
+        output += "x";
+    } else if(element.degree > 1) {
+        output += "x^" + to_string(element.degree);
+    }
+    return output;
+}
+
+string outputFirstElement(const PolyElement& element, string sign) {
+    string output;
+    if(element.coefficent >= 0) {
+        output =  " ";
+    } else {
+        output = sign + " ";
+    }
+    output += BaseOutputElement(element);
+    return output;
+}
+
+string outputElement(const PolyElement& element, string sign) {
+    string output;
+    output = " " + sign + " ";
+    output += BaseOutputElement(element);
+    return output;
+}
+
+ostream& operator<<(ostream& out, const Poly& poly){
+    list<PolyElement>::iterator it;
+    list<PolyElement> outputList = getListToPrint(poly);
+    if(outputList.size() == 0) {
+        out << 0;
+    }
+    string sign;
+    for(it = outputList.begin(); it != outputList.end(); ++it) {
+        sign = '+';
+        if((*it).coefficent != 0) {
+
+            if((*it).coefficent < 0) {
+                sign = '-';
+            }
+            if((&(*it) == &(outputList.front()))) {
+                out << outputFirstElement((*it), sign);
+            } else {
+                out << outputElement((*it), sign);
+            }
         }
     }
     return out;
--- PolynomialClass/Poly.h
+++ PolynomialClass/Poly.h
@@ -6,9 +6,9 @@ using namespace std;
 
 class PolyElement {
   public:      
-    PolyElement(double coefficent = 0, int degree = 0);
     int degree;
     double coefficent;
+    PolyElement(double coefficent = 0, int degree = 0);
     PolyElement& operator=(double coefficent);
 };
 
@@ -18,10 +18,17 @@ class Poly {
     Poly(double coefficent);
     Poly(const Poly& p);
     PolyElement& operator[](int degree);
-    void display();
-
+    double operator()(double xval) const;
+    Poly operator-() const;
   private:
     list<PolyElement> elements;
     size_t numberOfElements;
+  friend list<PolyElement> getListToPrint(const Poly& poly);
   friend ostream& operator<<(ostream& out, const Poly& poly);
+  friend Poly operator+(const Poly& p1, const Poly& p2);
+  friend Poly operator-(const Poly& p1, const Poly& p2);
+  friend Poly operator*(const Poly& p1, const Poly& p2);
 };
+string BaseOutputElement(const PolyElement& element);
+string outputFirstElement(const PolyElement& element, string sign);
+string outputElement(const PolyElement& element, string sign);
