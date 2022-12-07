c2bc8dc5224542a40c255e598f0645161f879e05
Aleksander Brzostowski <olekbrzost@gmail.com>
2022-12-07 18:20:13 +0100

    All test passed, seems finished.

--- MatrixClass/matrix.h
+++ MatrixClass/matrix.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <iostream>
-#include <math.h>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-class DiffrentMatrixesDimentionsException : public exception {
+class MatrixReferenceHandler;
+class Matrix;
+
+class InvalidMatrixSizesException : public exception {
   public:
     const char* what() const throw() {
-      return "Matrixes have diffrent dimensions";
+      return "Matrixes have dimensions invalid for performed operation.";
     }
 };
 
@@ -18,12 +22,24 @@ class MatrixIndexOutOfBoundariesException : public exception {
     }
 };
 
+class FailedToOpenFileException : public exception {
+  public:
+    const char* what() const throw() {
+      return "File is not open.";
+    }
+};
 
+class InvalidDataInFileException : public exception {
+  public:
+    const char* what() const throw() {
+      return "Data in file does not match matrix dimensions.";
+    }
+};
 
 class Matrix {
   public:
     Matrix(size_t numberOfRows = 1, size_t numberOfColumns = 1);
-    Matrix(string file);
+    Matrix(string fileName);
     Matrix(const Matrix& matrix);
     ~Matrix();
     size_t getNumberOfRows() const;
@@ -34,22 +50,26 @@ class Matrix {
     Matrix& operator+=(const Matrix& matrix);
     Matrix& operator-=(const Matrix& matrix);
     Matrix& operator*=(const Matrix& matrix);
+    friend Matrix matrixMultiplication(const Matrix& matrix1, const Matrix& matrix2);
+    friend Matrix numberMultiplication(const Matrix& matrix, double number);
     friend Matrix operator+(const Matrix& matrix1, const Matrix& matrix2);
     friend Matrix operator-(const Matrix& matrix1, const Matrix& matrix2);
     friend Matrix operator*(const Matrix& matrix1, const Matrix& matrix2);
     friend bool operator==(const Matrix& matrix1, const Matrix& matrix2);
     friend bool operator!=(const Matrix& matrix1, const Matrix& matrix2);
     friend ostream& operator<<(ostream& out, const Matrix& matrix);
+    friend class MatrixReferenceHandler;
     MatrixReferenceHandler operator()(size_t rowNumber, size_t columnNumber);
   private:
     struct MatrixData;
     MatrixData* data;
-    double read(size_t rowNumber, size_t columnNumber) const;
+    double reading(size_t rowNumber, size_t columnNumber) const;
+    void writing(size_t rowNumber, size_t columnNumber, double newValue);
 };
 
 struct Matrix::MatrixData {
   size_t numberOfRows;
-  size_t numberOfColums;
+  size_t numberOfColumns;
   double** elements;
   size_t numberOfReferences;
 
@@ -59,12 +79,11 @@ struct Matrix::MatrixData {
 };
 
 class MatrixReferenceHandler {
-  private:
-    Matrix* matrix;
-    size_t numberOfRows;
-    size_t numberOfColumns;
   public:
-  MatrixReferenceHandler(Matrix* matrix, size_t numberOfRows, size_t numberOfColumns);
-  operator double() const;
-  void operator=(double number);
+    MatrixReferenceHandler(Matrix* matrix, size_t row, size_t column);
+    operator double() const;
+    void operator=(double number);
+    Matrix* matrix;
+    size_t row;
+    size_t column;
 };
--- MatrixClass/testMatrix.cpp
+++ MatrixClass/testMatrix.cpp
@@ -1,1 +1,209 @@
- 
+#include "matrix.h"
+
+using namespace std;
+
+void testCompoundAddition();
+void testCompoundSubstraction();
+void testCompoundMultiplication();
+void testCompoundOperators();
+void testAddition(const Matrix& m1, const Matrix& m2);
+void testSubstraction(const Matrix& m1, const Matrix& m2);
+void testMultiplication(const Matrix& m1, const Matrix& m2);
+void testRegularOperators();
+void testExceptions();
+void testInvalidMatrixSizesException();
+void testMatrixIndexOutOfBoundariesException();
+void testFailedToOpenFileException();
+void testInvalidDataInFileException();
+void testReferenceCounting();
+
+int main() {
+    testCompoundOperators();
+    testRegularOperators();
+    testExceptions();
+    testReferenceCounting();
+}
+
+void testCompoundAddition() {
+    cout << "Compound addition: ";
+    Matrix m1(2, 2);
+    m1(0, 0) = 1;
+    m1(0, 1) = 3;
+    m1(1, 0) = 5;
+    m1(1, 1) = 7;
+    Matrix m2(2, 2);
+    m2(0, 0) = 2;
+    m2(0, 1) = 4;
+    m2(1, 0) = 6;
+    m2(1, 1) = 8;
+    Matrix properResult(2, 2);
+    properResult(0, 0) = 3;
+    properResult(0, 1) = 7;
+    properResult(1, 0) = 11;
+    properResult(1, 1) = 15;
+    m1 += m2;
+    if (m1 != properResult) {
+        cout << "FAILED" << endl;
+    }
+    else {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testCompoundSubstraction() {
+    cout << "Compound substraction: ";
+    Matrix m1("m1.txt");
+    Matrix m2("m2.txt");
+    Matrix properResult(2,2);
+    properResult(0, 0) = -4;
+    properResult(0, 1) = -4;
+    properResult(1, 0) = -4;
+    properResult(1, 1) = -4;
+    m1 -= m2;
+    if (m1 != properResult) {
+        cout << "FAILED" << endl;
+    } else {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testCompoundMultiplication() {
+    cout << "Compound multiplication: ";
+    Matrix m1("m1.txt");
+    Matrix m2("m2.txt");
+    Matrix properResult(2,2);
+    properResult(0, 0) = 19;
+    properResult(0, 1) = 22;
+    properResult(1, 0) = 43;
+    properResult(1, 1) = 50;
+    m1 *= m2;
+    if (m1 != properResult) {
+        cout << "FAILED" << endl;
+    } else {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testCompoundOperators() {
+    testCompoundAddition();
+    testCompoundSubstraction();
+    testCompoundMultiplication();
+}
+
+void testAddition(const Matrix& m1, const Matrix& m2) {
+    cout << "Regular addition: ";
+    Matrix properResult(2,2);
+    properResult(0, 0) = 6;
+    properResult(0, 1) = 8;
+    properResult(1, 0) = 10;
+    properResult(1, 1) = 12;
+    if ((m1 + m2) != properResult) {
+        cout << "FAILED" << endl;
+    } else {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testSubstraction(const Matrix& m1, const Matrix& m2) {
+    cout << "Regular substraction: ";
+    Matrix properResult(2,2);
+    properResult(0, 0) = -4;
+    properResult(0, 1) = -4;
+    properResult(1, 0) = -4;
+    properResult(1, 1) = -4;
+    if ((m1 - m2) != properResult) {
+        cout << "FAILED" << endl;
+    } else {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testMultiplication(const Matrix& m1, const Matrix& m2) {
+    cout << "Regular multiplication: ";
+    Matrix properResult(2,2);
+    properResult(0, 0) = 19;
+    properResult(0, 1) = 22;
+    properResult(1, 0) = 43;
+    properResult(1, 1) = 50;
+    if ((m1 * m2) != properResult) {
+        cout << "FAILED" << endl;
+    } else {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testRegularOperators() {
+    Matrix m1("m1.txt");
+    Matrix m2("m2.txt");
+    testAddition(m1, m2);
+    testSubstraction(m1, m2);
+    testMultiplication(m1, m2);
+}
+
+void testExceptions() {
+    testInvalidMatrixSizesException();
+    testMatrixIndexOutOfBoundariesException();
+    testFailedToOpenFileException();
+    testInvalidDataInFileException();
+}
+
+void testInvalidMatrixSizesException() {
+    cout << "InvalidMatrixSizesException: ";
+    Matrix m1(2, 1);
+    Matrix m2(1, 2);
+    try {
+        m1 += m2;
+        cout << "FAILED" << endl;
+    } catch (InvalidMatrixSizesException& e) {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testMatrixIndexOutOfBoundariesException() {
+    cout << "MatrixIndexOutOfBoundariesException: ";
+    Matrix m(1, 2);
+    try {
+        m(1, 2);
+        cout << "FAILED" << endl;
+    } catch (MatrixIndexOutOfBoundariesException& e) {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testFailedToOpenFileException() {
+    cout << "FailedToOpenFileException: ";
+    try {
+        Matrix m("thisFileDoesNotExists.txt");
+        cout << "FAILED" << endl;
+    } catch (FailedToOpenFileException& e) {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testInvalidDataInFileException() {
+    cout << "InvalidDataInFileException: ";
+    try {
+        Matrix m("5x5InvalidMatrix.txt");
+        cout << "FAILED" << endl;
+    } catch (InvalidDataInFileException& e) {
+        cout << "PASSED" << endl;
+    }
+}
+
+void testReferenceCounting() {
+    cout << "Reference counting: ";
+    Matrix m1("m1.txt");
+    Matrix m2("m2.txt");
+    Matrix m3(m1);
+    m2 = m3;
+    if(m1.getNumberOfReferences() != 3) {
+        cout << "FAILED" << endl;
+        return;
+    }
+    m2(0, 1) = 16;
+    if(m1.getNumberOfReferences() != 2) {
+        cout << "FAILED" << endl;
+    } else {
+        cout << "PASSED" << endl;
+    }
+}
